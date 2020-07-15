df455fe31b9e6cb04b1df07d282739de44e4c0c0
blue0802 <quocbaoit0802@gmail.com>
2020-07-15 23:23:09 +0700

    add lession 3

---
+++ express/lession-3/index.js
@@ -0,0 +1,39 @@
+const express = require('express');
+
+const app = express();
+
+const port = process.env.port || 3000;
+
+app.set('views', './views');
+app.set('view engine', 'pug');
+
+var items = [
+    {id: 1, todo: 'Di cho'},
+    {id: 2, todo: 'Nau com'},
+    {id: 3, todo: 'Rua bat'},
+    {id: 4, todo: 'Hoc code tai CodersX'},
+]
+
+app.get('/', (req, res) => {
+    res.send('<h1>I love CodersX</h1>');
+})
+
+app.get('/todos', (req, res) => {
+    res.render('todos', {
+        items: items
+    });
+})
+
+app.get('/todos/search', (req, res) => {
+    let q = req.query.q;
+    let matchesItems = items.filter(item => {
+        return item.todo.toLowerCase().indexOf(q.toLowerCase()) !== -1;
+    })
+    res.render('todos', {
+        items: matchesItems
+    })
+})
+
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}`);
+})
