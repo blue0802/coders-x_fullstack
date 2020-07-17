aacee21a58205969602ac0588c257c77657875e9
blue0802 <quocbaoit0802@gmail.com>
2020-07-17 21:41:23 +0700

    add lession 7 part 2

---
+++ express/lession-7-p2/index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const userRoute = require('./routes/user.route')
+const bookRoute = require('./routes/book.route')
+
+const app = express();
+
+const port = process.env.port || 3000;
+
+app.set('views', './views');
+app.set('view engine', 'pug');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use('/users', userRoute);
+app.use('/books', bookRoute);
+
+//initial home page
+app.get('/', (req, res) => {
+    res.render('index');
+})
+
+app.listen(port, () => {
+    console.log(`Server listening at http://localhost:${port}`);
+})
